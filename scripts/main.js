8e135ce1c5de0be39d1665a087920b6a938f208b
Thai Nguyen <nvthai.fr@gmail.com>
2020-05-08 16:30:04 +0700

    key styles + hint

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,27 @@
+const getKeyName = (keyValue) => {
+  switch (keyValue) {
+    case " ":
+      return "Space";
+    default:
+      return keyValue;
+  }
+};
+
 const onKeyDown = (event) => {
   const keyCode = event.which || event.keyCode;
   const keyValue = event.key;
-//   console.log(event);
+
   document.body.innerHTML = `
     <h1>${keyCode}</h1>
-    <span>${keyValue}</span>
-`
+    <kbd>${getKeyName(keyValue)}</kbd>
+  `;
+
+  console.log(event);
 };
 
-document.body.innerHTML= `<h1>Press any key</h1>`
+document.body.innerHTML = `
+    <h1>Press any key</h1>
+    <span><strong>Tips:</strong> See console log for key detail ;)</span>
+`;
+
 document.body.addEventListener("keydown", (event) => onKeyDown(event));
